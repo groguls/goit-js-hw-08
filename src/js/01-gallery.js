ff4f101046bb2809193e4d7992934fa9ab4b4905
groguls <groguls@gmail.com>
2023-06-22 23:45:48 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+
+const galleryEL = document.querySelector('.gallery');
+
+galleryEL.innerHTML = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryArr) {
+  return galleryArr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>
+
+  `
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
