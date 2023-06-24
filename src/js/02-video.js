cc3dca6a66075c90da57efdd2afbfed7317d930a
groguls <groguls@gmail.com>
2023-06-24 22:15:16 +0200

    task-02 added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.setCurrentTime(setResumeVimeoPlay());
+player.on('timeupdate', throttle(onVimeoPlay, 1000));
+
+function onVimeoPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+function setResumeVimeoPlay() {
+  const time = localStorage.getItem('videoplayer-current-time');
+  return JSON.parse(time).seconds ?? 0;
+}
