8692feb70076f181e2852fd3fea861f1dd00e16f
groguls <groguls@gmail.com>
2023-06-27 23:53:53 +0200

    task-02 updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,18 @@
-import Player from '@vimeo/player';
 import { saveToLocalStorage, loadFromLocalStorage } from './storage';
 const throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
 
 const localStorageKey = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 player.setCurrentTime(setResumeVimeoPlay());
-player.on('timeupdate', onVimeoPlay);
+player.on('timeupdate', throttle(onVimeoPlay, 1000));
 
 function onVimeoPlay(data) {
-  console.log(data);
-  saveToLocalStorage(localStorageKey, JSON.stringify(data));
+  saveToLocalStorage(localStorageKey, data.seconds);
 }
 
 function setResumeVimeoPlay() {
-  const time = loadFromLocalStorage(localStorageKey);
-  return JSON.parse(time).seconds ?? 0;
+  return loadFromLocalStorage(localStorageKey) ?? 0;
 }
